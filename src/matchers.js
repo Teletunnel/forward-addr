c2c6504cccefe45648d70dc6d6a909f7b4f06136
Maciej Krüger <mkg20001@gmail.com>
2018-08-02 10:56:57 +0200

    misc: Fix deps

--- src/matchers.js
+++ src/matchers.js
@@ -5,7 +5,7 @@ const minimatch = require('minimatch')
 module.exports = {
   strict: (value, compareWith) => value === compareWith,
   regex: (value, compareWith) => {
-    let [regex, flags] = String(compareWith).split('\\\/')
+    let [regex, flags] = String(compareWith).split('\\/')
     regex = new RegExp(regex, flags)
     return Boolean(regex.exec(value))
   },
